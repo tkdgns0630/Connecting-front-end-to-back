45931cd7df2697bd4d1590e5f81d851e878bfdbe
tkdgns0630 <129707996+tkdgns0630@users.noreply.github.com>
2023-08-07 15:19:27 +1000

    update for comments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
+// import express
 const express = require("express");
-
+// import note router
 const noteRouter = require("./notes");
-
+// instantaniate app  = express
 const app = express();
-
+// allow /notes route to be using note router
 app.use("/notes", noteRouter);
-
+// export
 module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,30 @@
+// initiating express path and modules
+
 const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
+//instantaniation of express
 const app = express();
 
+// allows files to be read in json format
 app.use(express.json());
+
+// instantaniate /api route with api module.
 app.use("/api", api);
-
+// publicly allow express to access folder public
 app.use(express.static("./Develop/public"));
-
+// set /note route to html
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
 );
-
+// set all other paths to be directed index.html
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "./Develop/public/assets/js/index.html"))
 );
-
+// start server.
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
